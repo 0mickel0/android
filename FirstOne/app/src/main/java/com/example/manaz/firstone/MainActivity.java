cda3090a4e35b7e43b0e17df0bdd82efda6d8bf5
Misha <m.a.nazarov@mail.ru>
2017-09-18 09:26:19 +0300

    init HellowWorld project

---
+++ FirstOne/app/src/main/java/com/example/manaz/firstone/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.manaz.firstone;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by manaz on 18.09.2017.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+}
