75c0d8278064d577404c20ee6018e662b1ba5018
Misha <m.a.nazarov@mail.ru>
2017-10-06 09:30:42 +0300

    Take a picture and holding it in ImageVIew

--- FirstOne/app/src/main/java/com/example/manaz/firstone/MainActivity.java
+++ FirstOne/app/src/main/java/com/example/manaz/firstone/MainActivity.java
@@ -1,18 +1,48 @@
 package com.example.manaz.firstone;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by manaz on 18.09.2017.
  */
 
-public class MainActivity extends Activity{
+public class MainActivity extends ActionBarActivity {
+
+    private Button btnCamera;
+    private ImageView capturedImage;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        btnCamera = (Button) findViewById(R.id.btnCamera);
+        capturedImage= (ImageView) findViewById(R.id.capturedImage);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK) {
+            Bitmap bp = (Bitmap) data.getExtras().get("data");
+            capturedImage.setImageBitmap(bp);
+        }
     }
 
 }
